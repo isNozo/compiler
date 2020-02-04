f9d0c74a8b4df6fd33b01803d25874113a8b2620
nozo <nozomi.pine@gmail.com>
2020-02-04 13:01:03 +0900

    imp trans_exp (WIP)

--- interp.ml
+++ interp.ml
@@ -1,15 +1,22 @@
 type id = string
-type op = Plus | Minus | Times | Div ;;
 type stm = Stems of stm * stm
          | Assign of id * exp
          | Print of exp
 and  exp = ID of id
          | Num of int
-         | Plus of exp * exp
-         | Minux of exp * exp
-         | Times of exp * exp
+         | Add of exp * exp
+         | Sub of exp * exp
+         | Mul of exp * exp
          | Div of exp * exp
 
-exception No_such_sybol
-let ev0 = fun _ -> raise No_such_symbol
+let e0 = fun _ -> None
 let update var vl env = fun v -> if (v = var) then vl else env v
+
+let trans_exp exp env =
+  match exp with
+      ID  var      -> env var
+    | Num n        -> Some n
+    | Add (e1, e2) -> None
+    | Sub (e1, e2) -> None
+    | Mul (e1, e2) -> None
+    | Div (e1, e2) -> None
