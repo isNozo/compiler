ed853784140158d525c8893cc3cc2dc75c668f94
nozo <nozomi.pine@gmail.com>
2020-03-02 14:37:26 +0000

    Add explicit type

--- interp.ml
+++ interp.ml
@@ -1,5 +1,5 @@
 type id = string
-type stm = Stems of stm * stm
+type stm = Stmts of stm * stm
          | Assign of id * exp
          | Print of exp
 and  exp = ID of id
@@ -9,14 +9,36 @@ and  exp = ID of id
          | Mul of exp * exp
          | Div of exp * exp
 
-let e0 = fun _ -> None
-let update var vl env = fun v -> if (v = var) then vl else env v
+let e0
+        : id -> exp option
+        = fun _ ->
+          None
 
-let trans_exp exp env =
-  match exp with
-      ID  var      -> env var
-    | Num n        -> Some n
-    | Add (e1, e2) -> None
-    | Sub (e1, e2) -> None
-    | Mul (e1, e2) -> None
-    | Div (e1, e2) -> None
+let update
+        : id -> exp -> (id -> exp option) -> (id -> exp option)
+        = fun var vl env ->
+          fun v -> if (v = var) then Some vl else env v
+
+let trans_stm
+        : stm -> exp option
+        = fun stm ->
+          match stm with
+            Stmts  (s1, s2) -> None
+          | Assign (var, e) -> None
+          | Print  e        -> None
+
+let trans_exp
+        : exp -> (id -> exp option) -> exp option 
+        = fun exp env ->
+          match exp with
+            ID  var      -> env var
+          | Num n        -> None
+          | Add (e1, e2) -> None
+          | Sub (e1, e2) -> None
+          | Mul (e1, e2) -> None
+          | Div (e1, e2) -> None
+
+let prog 
+        : stm
+        = Stmts (Assign ("x", Add (Num 1, Mul (Num 2, Num 3))),
+            Stmts (Assign ("y", Div (ID "x", Num 4)), Print (ID "y")))
