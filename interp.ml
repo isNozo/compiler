fe451f6400bc21be6a416d10161910ae5c60935e
nozo <nozomi.pine@gmail.com>
2020-03-03 12:25:10 +0000

    Fix indent

--- interp.ml
+++ interp.ml
@@ -10,35 +10,35 @@ and  exp = ID of id
          | Div of exp * exp
 
 let e0
-        : id -> exp option
-        = fun _ ->
-          None
+    : id -> exp option
+  = fun _ ->
+  None
 
 let update
-        : id -> exp -> (id -> exp option) -> (id -> exp option)
-        = fun var vl env ->
-          fun v -> if (v = var) then Some vl else env v
+    : id -> exp -> (id -> exp option) -> (id -> exp option)
+  = fun var vl env ->
+  fun v -> if (v = var) then Some vl else env v
 
 let trans_stm
-        : stm -> exp option
-        = fun stm ->
-          match stm with
-            Stmts  (s1, s2) -> None
-          | Assign (var, e) -> None
-          | Print  e        -> None
+    : stm -> exp option
+  = fun stm ->
+  match stm with
+  | Stmts  (s1, s2) -> None
+  | Assign (var, e) -> None
+  | Print  e        -> None
 
 let trans_exp
-        : exp -> (id -> exp option) -> exp option 
-        = fun exp env ->
-          match exp with
-            ID  var      -> env var
-          | Num n        -> None
-          | Add (e1, e2) -> None
-          | Sub (e1, e2) -> None
-          | Mul (e1, e2) -> None
-          | Div (e1, e2) -> None
+    : exp -> (id -> exp option) -> exp option 
+  = fun exp env ->
+  match exp with
+  | ID  var      -> env var
+  | Num n        -> None
+  | Add (e1, e2) -> None
+  | Sub (e1, e2) -> None
+  | Mul (e1, e2) -> None
+  | Div (e1, e2) -> None
 
 let prog 
-        : stm
-        = Stmts (Assign ("x", Add (Num 1, Mul (Num 2, Num 3))),
-            Stmts (Assign ("y", Div (ID "x", Num 4)), Print (ID "y")))
+    : stm
+  = Stmts (Assign ("x", Add (Num 1, Mul (Num 2, Num 3))),
+           Stmts (Assign ("y", Div (ID "x", Num 4)), Print (ID "y")))
